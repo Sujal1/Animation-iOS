6576cc0af6ed277ff44bace11a6456822dbf369d
Sujal Manandhar <sujal.manandhar1@gmail.com>
2019-03-09 17:55:47 +0545

    Start stop animation

--- LottieAnimation/ViewController.swift
+++ LottieAnimation/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    let animationView  = LOTAnimationView(name: "loader")
+    
+    var isAnimating: Bool!
+    
+    @IBOutlet weak var buttonAnimate: UIButton!
+    
+    @IBAction func beginAnimation(_ sender: Any) {
+        if isAnimating {
+            animationView.stop()
+            animationView.removeFromSuperview()
+            buttonAnimate.setTitle("Animate", for: .normal)
+        } else {
+            view.addSubview(animationView)
+            animationView.play()
+            buttonAnimate.setTitle("Stop", for: .normal)
+        }
+        isAnimating = !isAnimating
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setUpInitialCondition()
+        setUpAnimationView()
     }
 
+    func setUpInitialCondition() {
+        isAnimating = false
+        buttonAnimate.setTitle("Animate", for: .normal)
+    }
+    
+    func setUpAnimationView() {
+        animationView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height - 50)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopAnimation = true
+    }
 
 }
 
