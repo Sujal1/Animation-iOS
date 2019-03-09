7e3c8dc261effad7aa2f29c4b6f612a2aaf61c6d
Sujal Manandhar <sujal.manandhar1@gmail.com>
2019-03-09 16:38:30 +0545

    Initial commit

---
+++ LottieAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieAnimation
+//
+//  Created by Sujal on 3/9/19.
+//  Copyright © 2019 Sujal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
